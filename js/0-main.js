e9ebc22afe7ac3e00096386ac39c273733035403
ivc_ShherbakovIV <ivc_ShherbakovIV@IVC-SHHERBAKOVI.esrr.oao.rzd>
2015-01-23 12:20:25 +0300

    todo for js

--- js/0-main.js
+++ js/0-main.js
@@ -1,18 +1,16 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-
 $(document).ready(function() {
     var pathname = window.location.pathname;
     pathname = pathname.replace(/^\/|\/$/g, '');
 
     alert(pathname);
+    //Проверяем авторизацию
+    
     //запрос чтобы взять данные по директории
     $.post('/ajax_handler.php', {fold: pathname, action: "LIST"}, function(data) {
-        alert(data);
+        //строим таблицу
     });
+    
+    //Подвязываем кнопки с действиями
+    
 
 });
