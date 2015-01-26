ff5aefd3d5a34b10d571a298914a55556f5c2f21
ivc_ShherbakovIV <ivc_ShherbakovIV@IVC-SHHERBAKOVI.esrr.oao.rzd>
2015-01-26 06:15:59 +0300

    Add: Navigation links

--- js/0-main.js
+++ js/0-main.js
@@ -29,16 +29,21 @@ $(document).ready(function() {
         var obj = jQuery.parseJSON(data);
         $.each(obj, function(key, value) {
             var arr = value.size.split(' ');
-            if (arr[1]=== undefined) arr[1]=" ";
+            if (arr[1] === undefined)
+                arr[1] = " ";
+            var url = window.location.href;
+            if (!url.match("/$"))
+                url += "/";
+            var link = url + encodeURIComponent(value.item);
             if (value.type === 'folder')
             {
-                
+
                 var add_html = '<div class="odd">' +
                         '<div class="cell flag">' +
                         '<input id="checkbox-' + value.item + '" class="checkbox" type="checkbox" name="">  ' +
                         '<label for="checkbox-' + value.item + '"></label> ' +
                         '</div>' +
-                        '<div class="cell name"><a class="folder" >' + value.item + '</a></div>' +
+                        '<div class="cell name"><a class="folder" href="' + link + '" >' + value.item + '</a></div>' +
                         '<div class="cell manage">' +
                         '<a class="clipboard tooltip"  data-title="Копировать ссылку в буфер обмена"></a><a class="rename tooltip"  data-title="Переименовать"></a>' +
                         '</div>' +
@@ -48,13 +53,13 @@ $(document).ready(function() {
 
             }
             else {
-                
+
                 var add_html = '<div class="even">' +
                         '<div class="cell flag">' +
                         '<input id="checkbox-' + value.item + '" class="checkbox" type="checkbox" name="">  ' +
                         '<label for="checkbox-' + value.item + '"></label> ' +
                         '</div>' +
-                        '<div class="cell name"><a class="file" >' + value.item + '</a></div>' +
+                        '<div class="cell name"><a class="file" href="' + link + '" >' + value.item + '</a></div>' +
                         '<div class="cell manage">' +
                         '<a class="clipboard tooltip"  data-title="Копировать ссылку в буфер обмена"></a><a class="rename tooltip"  data-title="Переименовать"></a>' +
                         '</div>' +
@@ -64,44 +69,45 @@ $(document).ready(function() {
             }
             $('.table').append(add_html);
         });
-    });
+        //Подвязываем кнопки с действиями
+        $(".clipboard").click(function() {
+            var item = getElementInfo(this).item;
+            var url = window.location.href;
+            if (!url.match("/$"))
+                url += "/";
+            var link = url + encodeURIComponent(item);
+            copyToClipboard(link);
+        });
 
-    //Подвязываем кнопки с действиями
-    $(".clipboard").click(function() {
-        var item = getElementInfo(this).item;
-        var url = window.location.href;
-        if (!url.match("/$"))
-            url += "/";
-        var link = url + encodeURIComponent(item);
-        copyToClipboard(link);
-    });
+        //Подвязываем кнопки с действиями
+        $(".rename").click(function() {
+            var el = getElementInfo(this);
+            var item = el.item;
+            var pathname = window.location.pathname;
+            pathname = pathname.replace(/^\/|\/$/g, '');
+            alert(pathname + " " + item);
+        });
 
-    //Подвязываем кнопки с действиями
-    $(".rename").click(function() {
-        var el = getElementInfo(this);
-        var item = el.item;
-        var pathname = window.location.pathname;
-        pathname = pathname.replace(/^\/|\/$/g, '');
-        alert(pathname + " " + item);
-    });
+        //Подвязываем кнопки с действиями
+        $(".create").click(function() {
+            var pathname = window.location.pathname;
+            pathname = pathname.replace(/^\/|\/$/g, '');
+            alert(pathname);
+        });
 
-    //Подвязываем кнопки с действиями
-    $(".create").click(function() {
-        var pathname = window.location.pathname;
-        pathname = pathname.replace(/^\/|\/$/g, '');
-        alert(pathname);
-    });
-
-    //Подвязываем кнопки с действиями
-    $(".remove").click(function() {
-        var pathname = window.location.pathname;
-        pathname = pathname.replace(/^\/|\/$/g, '');
-        $.each($('.table').find('.checkbox'), function(key, value) {
-            if ($(value).prop('checked'))
-            {
-                var el = getElementInfo(value);
-                alert(pathname + " " + el.item);
-            }
+        //Подвязываем кнопки с действиями
+        $(".remove").click(function() {
+            var pathname = window.location.pathname;
+            pathname = pathname.replace(/^\/|\/$/g, '');
+            $.each($('.table').find('.checkbox'), function(key, value) {
+                if ($(value).prop('checked'))
+                {
+                    var el = getElementInfo(value);
+                    alert(pathname + " " + el.item);
+                }
+            });
         });
     });
+
+
 });
