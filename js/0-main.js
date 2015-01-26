641663c3f21b10087c4e023743051c87d50ae3b3
ivc_ShherbakovIV <ivc_ShherbakovIV@IVC-SHHERBAKOVI.esrr.oao.rzd>
2015-01-26 05:57:31 +0300

    Add: Folder browse

--- js/0-main.js
+++ js/0-main.js
@@ -19,14 +19,51 @@ function copyToClipboard(text) {
 $(document).ready(function() {
     var pathname = window.location.pathname;
     pathname = pathname.replace(/^\/|\/$/g, '');
-            
+
 
     //alert(pathname);
     //Проверяем авторизацию
 
     //запрос чтобы взять данные по директории
     $.post('/ajax_handler.php', {fold: pathname, action: "LIST"}, function(data) {
-        alert(data);
+        var obj = jQuery.parseJSON(data);
+        $.each(obj, function(key, value) {
+            var arr = value.size.split(' ');
+            if (arr[1]=== undefined) arr[1]=" ";
+            if (value.type === 'folder')
+            {
+                
+                var add_html = '<div class="odd">' +
+                        '<div class="cell flag">' +
+                        '<input id="checkbox-' + value.item + '" class="checkbox" type="checkbox" name="">  ' +
+                        '<label for="checkbox-' + value.item + '"></label> ' +
+                        '</div>' +
+                        '<div class="cell name"><a class="folder" >' + value.item + '</a></div>' +
+                        '<div class="cell manage">' +
+                        '<a class="clipboard tooltip"  data-title="Копировать ссылку в буфер обмена"></a><a class="rename tooltip"  data-title="Переименовать"></a>' +
+                        '</div>' +
+                        '<div class="cell size"><span>' + arr[0] + '</span> ' + arr[1] + '</div>' +
+                        '<div class="cell date">' + value.time + '</div>' +
+                        '</div>';
+
+            }
+            else {
+                
+                var add_html = '<div class="even">' +
+                        '<div class="cell flag">' +
+                        '<input id="checkbox-' + value.item + '" class="checkbox" type="checkbox" name="">  ' +
+                        '<label for="checkbox-' + value.item + '"></label> ' +
+                        '</div>' +
+                        '<div class="cell name"><a class="file" >' + value.item + '</a></div>' +
+                        '<div class="cell manage">' +
+                        '<a class="clipboard tooltip"  data-title="Копировать ссылку в буфер обмена"></a><a class="rename tooltip"  data-title="Переименовать"></a>' +
+                        '</div>' +
+                        '<div class="cell size"><span>' + arr[0] + '</span> ' + arr[1] + '</div>' +
+                        '<div class="cell date">' + value.time + '</div>' +
+                        '</div>';
+            }
+            $('.table').append(add_html);
+        });
     });
 
     //Подвязываем кнопки с действиями
@@ -62,9 +99,9 @@ $(document).ready(function() {
         $.each($('.table').find('.checkbox'), function(key, value) {
             if ($(value).prop('checked'))
             {
-                var el=getElementInfo(value);
-                alert(pathname+" "+el.item);
-            }           
+                var el = getElementInfo(value);
+                alert(pathname + " " + el.item);
+            }
         });
     });
 });
