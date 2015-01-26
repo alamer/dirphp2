0dc4278ab006ab30c591d06dd549d91efe598ffa
ivc_ShherbakovIV <ivc_ShherbakovIV@IVC-SHHERBAKOVI.esrr.oao.rzd>
2015-01-26 12:27:33 +0300

    Change: Code refactoring

--- js/0-main.js
+++ js/0-main.js
@@ -75,7 +75,59 @@ DIRJS.copyToClipboard = function(text) {
 };
 
 DIRJS.loadFolderList = function() {
+    $.post('/ajax_handler.php', {fold: DIRJS.pathname, action: "LIST"}, function(data) {
+        if (data !== 'null') {
+            var obj = jQuery.parseJSON(data);
+            $.each(obj, function(key, value) {
+                var arr = value.size.split(' ');
+                if (arr[1] === undefined)
+                    arr[1] = " ";
+                var url = window.location.href;
+                if (!url.match("/$"))
+                    url += "/";
+                var link = url + encodeURIComponent(value.item);
+                if (value.type === 'folder')
+                {
 
+                    var add_html = '<div class="odd">' +
+                            '<div class="cell flag">' +
+                            '<input id="checkbox-' + value.item + '" class="checkbox" type="checkbox" name="">  ' +
+                            '<label for="checkbox-' + value.item + '"></label> ' +
+                            '</div>' +
+                            '<div class="cell name"><a class="folder" href="' + link + '" >' + value.item + '</a></div>' +
+                            '<div class="cell manage">' +
+                            '<a class="clipboard tooltip"  data-title="Копировать ссылку в буфер обмена"></a><a class="rename tooltip"  data-title="Переименовать"></a>' +
+                            '</div>' +
+                            '<div class="cell size"><span>' + arr[0] + '</span> ' + arr[1] + '</div>' +
+                            '<div class="cell date">' + value.time + '</div>' +
+                            '</div>';
+
+                }
+                else {
+
+                    var add_html = '<div class="even">' +
+                            '<div class="cell flag">' +
+                            '<input id="checkbox-' + value.item + '" class="checkbox" type="checkbox" name="">  ' +
+                            '<label for="checkbox-' + value.item + '"></label> ' +
+                            '</div>' +
+                            '<div class="cell name"><a class="file" href="' + link + '" >' + value.item + '</a></div>' +
+                            '<div class="cell manage">' +
+                            '<a class="clipboard tooltip"  data-title="Копировать ссылку в буфер обмена"></a><a class="rename tooltip"  data-title="Переименовать"></a>' +
+                            '</div>' +
+                            '<div class="cell size"><span>' + arr[0] + '</span> ' + arr[1] + '</div>' +
+                            '<div class="cell date">' + value.time + '</div>' +
+                            '</div>';
+                }
+                $('.table').append(add_html);
+            });
+        }
+
+        DIRJS.clipboardBind();
+
+        //@TODO rename bind
+        DIRJS.renameBind();
+        DIRJS.isLoggedIn();
+    });
 };
 
 DIRJS.createFolderBind = function() {
@@ -172,213 +224,72 @@ DIRJS.removeBind = function()
         $("#remove").css("display", "none");
     });
 };
-/*
- function DIRJS.getPathname()
- {
- var pathname = window.location.pathname;
- return pathname = pathname.replace(/^\/|\/$/g, '');
- }
- 
- 
- function DIRJS.getElementInfo(el)
- {
- var strdiv = $(el).parent().closest('div').parent();
- var item = strdiv.find(".name").text();
- var type = $(el).parent().closest('div').parent().attr('class');
- var res = new Object();
- res.item = item;
- res.type = type;
- return res;
- }
- 
- 
- function DIRJS.initParentRef()
- {
- var url = window.location.href;
- var pathname = DIRJS.getPathname();
- if (!url.match("/$"))
- url += "/";
- if (pathname == "")
- {
- $(".parent").hide();
- }
- else
- {
- result = url.replace(/\/[^\/]+\/?$/ig, "");
- $(".parent").attr('href', result);
- }
- }
- 
- function DIRJS.clearCookies()
- {
- $.cookie("username", null, {path: '/'});
- }
- 
- 
- function DIRJS.isLoggedIn()
- {
- 
- if (typeof $.cookie('username') === 'undefined' || $.cookie('username') == 'null') {
- DIRJS.hideAll();
- }
- else
- {
- DIRJS.showAll();
- }
- 
- }
- 
- function DIRJS.hideAll()
- {
- $(".rename").hide();
- $(".create").hide();
- $(".remove").hide();
- $(".upload").hide();
- $(".flag").hide();
- 
- $("#logout").hide();
- $("#login").show();
- }
- 
- function DIRJS.showAll()
- {
- $(".rename").show();
- $(".create").show();
- $(".remove").show();
- $(".upload").show();
- $(".flag").show();
- $("#logout").show();
- $("#login").hide();
- }
- 
- function DIRJS.copyToClipboard(text) {
- window.prompt("Copy to clipboard: Ctrl+C, Enter", text);
- }*/
 
-$(document).ready(function() {
-    DIRJS.pathname = DIRJS.getPathname();
-    //var pathname = DIRJS.getPathname();
-    DIRJS.isLoggedIn();
-    DIRJS.initParentRef();
-    //alert(pathname);
-    //Проверяем авторизацию
 
-    //запрос чтобы взять данные по директории
-    $.post('/ajax_handler.php', {fold: DIRJS.pathname, action: "LIST"}, function(data) {
-        if (data !== 'null') {
-            var obj = jQuery.parseJSON(data);
-            $.each(obj, function(key, value) {
-                var arr = value.size.split(' ');
-                if (arr[1] === undefined)
-                    arr[1] = " ";
-                var url = window.location.href;
-                if (!url.match("/$"))
-                    url += "/";
-                var link = url + encodeURIComponent(value.item);
-                if (value.type === 'folder')
+DIRJS.clipboardBind = function() {
+    var copy_sel = $('.clipboard');
+    // Disables other default handlers on click (avoid issues)
+    copy_sel.on('click', function(e) {
+        e.preventDefault();
+    });
+    // Apply clipboard click event
+    copy_sel.clipboard({
+        path: '/js/jquery.clipboard.swf',
+        copy: function() {
+            var this_sel = $(this);
+
+            // Hide "Copy" and show "Copied, copy again?" message in link
+            var item = DIRJS.getElementInfo(this).item;
+            var url = window.location.href;
+            if (!url.match("/$"))
+                url += "/";
+            var link = url + encodeURIComponent(item);
+            // Return text in closest element (useful when you have multiple boxes that can be copied)
+            return link;
+        }
+    });
+};
+
+
+DIRJS.renameBind = function() {
+    //Подвязываем кнопки с действиями
+    $(".rename").click(function() {
+        var el = DIRJS.getElementInfo(this);
+        var item = el.item;
+        $(".original").html(item);
+        $(".new").val(item);
+        //var pathname = DIRJS.getPathname();
+        $("#rename").css("display", "block");
+        //alert(pathname + " " + item);
+    });
+    $("#rename_ok").click(function() {
+        var pathname = DIRJS.getPathname();
+        var item = $(".new").val();
+        if (item !== "") {
+            $.post('/ajax_handler.php', {fold: pathname, olditem: $(".original").html(), newitem: item, action: "RENAME"}, function(data) {
+                if (data == "")
                 {
-
-                    var add_html = '<div class="odd">' +
-                            '<div class="cell flag">' +
-                            '<input id="checkbox-' + value.item + '" class="checkbox" type="checkbox" name="">  ' +
-                            '<label for="checkbox-' + value.item + '"></label> ' +
-                            '</div>' +
-                            '<div class="cell name"><a class="folder" href="' + link + '" >' + value.item + '</a></div>' +
-                            '<div class="cell manage">' +
-                            '<a class="clipboard tooltip"  data-title="Копировать ссылку в буфер обмена"></a><a class="rename tooltip"  data-title="Переименовать"></a>' +
-                            '</div>' +
-                            '<div class="cell size"><span>' + arr[0] + '</span> ' + arr[1] + '</div>' +
-                            '<div class="cell date">' + value.time + '</div>' +
-                            '</div>';
-
+                    location.reload();
                 }
-                else {
-
-                    var add_html = '<div class="even">' +
-                            '<div class="cell flag">' +
-                            '<input id="checkbox-' + value.item + '" class="checkbox" type="checkbox" name="">  ' +
-                            '<label for="checkbox-' + value.item + '"></label> ' +
-                            '</div>' +
-                            '<div class="cell name"><a class="file" href="' + link + '" >' + value.item + '</a></div>' +
-                            '<div class="cell manage">' +
-                            '<a class="clipboard tooltip"  data-title="Копировать ссылку в буфер обмена"></a><a class="rename tooltip"  data-title="Переименовать"></a>' +
-                            '</div>' +
-                            '<div class="cell size"><span>' + arr[0] + '</span> ' + arr[1] + '</div>' +
-                            '<div class="cell date">' + value.time + '</div>' +
-                            '</div>';
+                else
+                {
+                    alert(data);
                 }
-                $('.table').append(add_html);
             });
         }
+        else
+        {
+            alert("New name cannot be emty");
+        }
 
-        var copy_sel = $('.clipboard');
-        // Disables other default handlers on click (avoid issues)
-        copy_sel.on('click', function(e) {
-            e.preventDefault();
-        });
-        // Apply clipboard click event
-        copy_sel.clipboard({
-            path: '/js/jquery.clipboard.swf',
-            copy: function() {
-                var this_sel = $(this);
-
-                // Hide "Copy" and show "Copied, copy again?" message in link
-                var item = DIRJS.getElementInfo(this).item;
-                var url = window.location.href;
-                if (!url.match("/$"))
-                    url += "/";
-                var link = url + encodeURIComponent(item);
-                // Return text in closest element (useful when you have multiple boxes that can be copied)
-                return link;
-            }
-        });
-
-        //Подвязываем кнопки с действиями
-        $(".rename").click(function() {
-            var el = DIRJS.getElementInfo(this);
-            var item = el.item;
-            $(".original").html(item);
-            $(".new").val(item);
-            //var pathname = DIRJS.getPathname();
-            $("#rename").css("display", "block");
-            //alert(pathname + " " + item);
-        });
-        $("#rename_ok").click(function() {
-            var pathname = DIRJS.getPathname();
-            var item = $(".new").val();
-            if (item !== "") {
-                $.post('/ajax_handler.php', {fold: pathname, olditem: $(".original").html(), newitem: item, action: "RENAME"}, function(data) {
-                    if (data == "")
-                    {
-                        location.reload();
-                    }
-                    else
-                    {
-                        alert(data);
-                    }
-                });
-            }
-            else
-            {
-                alert("New name cannot be emty");
-            }
-
-        });
-        $("#rename_cancel").click(function() {
-            $("#rename").css("display", "none");
-        });
-
-        DIRJS.isLoggedIn();
     });
+    $("#rename_cancel").click(function() {
+        $("#rename").css("display", "none");
+    });
+};
 
-
-    DIRJS.authBind();
-
-    DIRJS.createFolderBind();
-
-    //@TODO remove
-    DIRJS.removeBind();
-
+DIRJS.uploaderBind = function()
+{
     var uploader = new ss.SimpleUpload({
         button: 'upload-btn', // file upload button
         url: '/uploadHandler.php', // server side handler
@@ -421,5 +332,21 @@ $(document).ready(function() {
             // do something with response...
         }
     });
+};
+
+DIRJS.init = function()
+{
+    DIRJS.pathname = DIRJS.getPathname();
+    DIRJS.isLoggedIn();
+    DIRJS.initParentRef();
+    DIRJS.loadFolderList();
+    DIRJS.authBind();
+    DIRJS.createFolderBind();
+    DIRJS.removeBind();
+    DIRJS.uploaderBind();
+};
+
+$(document).ready(function() {
+    DIRJS.init();
 
 });
